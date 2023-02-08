3a9e66a6a65f1879afe5ee01a0c90d64690e0e0f
chanwoo <chanwoo0628@gmail.com>
2023-02-08 20:38:50 +0900

    Add : 디스코드 메시지에 추가할 직업 별 아이콘 링크 추가함

---
+++ SearchTools/classIcon.py
@@ -0,0 +1,28 @@
+# 직업 추가마다 아이콘 추가하기
+
+classIcon = { 
+    '디스트로이어' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_destroyer.png',
+    '워로드' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_warlord.png',
+    '버서커' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_berserker.png',
+    '홀리나이트' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_holyknight.png',
+    '슬레이어' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_berserker_female.png',
+    '배틀마스터' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_battle_master.png',
+    '인파이터' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_infighter.png',
+    '기공사' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_force_master.png',
+    '창술사' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_lance_master.png',
+    '스트라이커' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_battle_master_male.png',
+    '데빌헌터' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_devil_hunter.png',
+    '블래스터' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_blaster.png',
+    '호크아이' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_hawk_eye.png',
+    '스카우터' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_scouter.png',
+    '건슬링어' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_devil_hunter_female.png',
+    '바드' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_bard.png',
+    '서머너' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_summoner.png',
+    '아르카나' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_arcana.png',
+    '소서리스' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_elemental_master.png',
+    '블레이드' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_blade.png',
+    '데모닉' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_demonic.png',
+    '리퍼' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_reaper.png',
+    '도화가' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_yinyangshi.png',
+    '기상술사' : 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/emblem_weather_artist.png'
+    }
