7b6cd08a41a5f4cf155950838a661aa6ca8d2ea3
chanwoo <chanwoo0628@gmail.com>
2023-02-20 01:40:32 +0900

    Add : Embed 메시지로 추가할 설명문 작성 메소드 추가함

---
+++ ExtractTools/DescriptionExtract.py
@@ -0,0 +1,10 @@
+# embed에 들어갈 description 작성 메소드
+# dict -> str
+
+def descriptionExtract(data: dict):
+    description = ''
+    
+    for key, value in zip(data.keys(), data.values()):
+        description += f'**{key}**  :  {value}\n'
+    
+    return description
